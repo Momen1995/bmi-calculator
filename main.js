21000f9497b22228ac96c6ac2e7950b5fbab5b7d
Momen1995 <momensrk@gmail.com>
2024-04-02 10:36:38 +0600

    calc update

--- main.js
+++ main.js
@@ -12,5 +12,18 @@ button.addEventListener("click",function(){
   const weight = Number(inputWeight.value)
 
   const calcHeightWeight = weight / (height * height);
-  result.textContent = calcHeightWeight.toFixed(2)
+  //result.textContent = calcHeightWeight.toFixed(2)
+
+  if(calcHeightWeight < 18.5 && calcHeightWeight > 0){
+    result.textContent = `Mr.${names} BMI result: ${calcHeightWeight.toFixed(2)}. It's Underweight😥`;
+  }
+  else if(calcHeightWeight > 18.5 && calcHeightWeight < 25){
+    result.textContent = `Mr.${names} BMI result: ${calcHeightWeight.toFixed(2)}. It's Normal weight🙂`;
+  }
+  else if (calcHeightWeight > 25 && calcHeightWeight < 30) {
+    result.textContent = `Mr.${names} BMI result: ${calcHeightWeight.toFixed(2)}. It's Overweight😕`;
+  } 
+  else if(calcHeightWeight > 30) {
+    result.textContent = `Mr.${names} BMI result: ${calcHeightWeight.toFixed(2)}. It's Obese😥`;
+  }
 })
